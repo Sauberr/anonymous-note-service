7fea154aff3f2e14b867235a4bfff45a53e9d392
sauberr <sauberr10@gmail.com>
2025-03-16 21:02:04 +0300

    Fix: Add sqladmin.

---
+++ app/admin/views.py
@@ -0,0 +1,43 @@
+from sqladmin import ModelView
+
+from app.users.models import User
+from app.notes.models import Note
+from app.authentication.model import AccessToken, OAuthAccount
+
+
+
+class UserAdmin(ModelView, model=User):
+    column_list = [u.name for u in User.__table__.columns]
+    column_details_exclude_list = [User.hashed_password]
+    can_delete = False
+    name = "User"
+    name_plural = "Users"
+    column_searchable_list = ["username", "email"]
+    column_filters = ["is_active", "is_superuser", "created_at"]
+    icon = "fa-solid fa-user"
+
+
+class NoteAdmin(ModelView, model=Note):
+    column_list = [n.name for n in Note.__table__.columns]
+    can_delete = False
+    name = "Note"
+    name_plural = "Notes"
+    column_searchable_list = ["title", "description"]
+    column_filters = ["is_ephemeral", "lifetime"]
+    icon = "fa-solid fa-sticky-note"
+
+
+class AccessTokensAdmin(ModelView, model=AccessToken):
+    column_list = [a.name for a in AccessToken.__table__.columns]
+    can_delete = False
+    name = "Access Token"
+    name_plural = "Access Tokens"
+    icon = "fa-solid fa-key"
+
+
+class OAuthAccountAdmin(ModelView, model=OAuthAccount):
+    column_list = [o.name for o in OAuthAccount.__table__.columns]
+    can_delete = False
+    name = "OAuth Account"
+    name_plural = "OAuth Accounts"
+    icon = "fa-solid fa-key"
--- app/main.py
+++ app/main.py
@@ -1,43 +1,54 @@
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends
 from fastapi.security import HTTPBearer
 from fastapi.staticfiles import StaticFiles
+from sqladmin import Admin
 
+from app.admin.views import NoteAdmin, UserAdmin, AccessTokensAdmin, OAuthAccountAdmin
 from app.authentication.messages import router as message_router
 from app.authentication.router import router as auth_router
 from app.core.config import settings
 from app.notes.router import router as note_router
 from app.users.router import router as user_router
 from app.webhooks.user import router as webhook_router
-from app.core.lifespan import lifespan
+from app.create_fastapi_app import create_app
+from app.core.db_session import engine
 
-app = FastAPI()
+main_app = create_app()
+main_app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+admin = Admin(
+    main_app,
+    engine,
+    title="FastAPI Admin",
+    base_url="/admin",
+    logo_url="/static/images/logo.png",
+)
+
+
+admin.add_view(UserAdmin)
+admin.add_view(NoteAdmin)
+admin.add_view(AccessTokensAdmin)
+admin.add_view(OAuthAccountAdmin)
 
 http_bearer = HTTPBearer(auto_error=True)
 
-
-app.include_router(
+main_app.include_router(
     note_router,
 )
-app.include_router(
+main_app.include_router(
     user_router,
     dependencies=[Depends(http_bearer)],
 )
-app.include_router(auth_router)
-app.include_router(message_router)
+main_app.include_router(auth_router)
+main_app.include_router(message_router)
 
-app.include_router(webhook_router)
-
-
-app.router.lifespan_context = lifespan
+main_app.include_router(webhook_router)
 
 
 if __name__ == "__main__":
-    print(settings.api.v1.auth)
     uvicorn.run(
-        app,
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
